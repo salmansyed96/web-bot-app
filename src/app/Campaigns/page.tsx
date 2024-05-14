a0ca13e63ac554f877287a0ef1bb855681105615
Syed Mohammad Salman <124539970+salmansyed96@users.noreply.github.com>
2024-05-14 13:51:08 +0530

    add rounting

---
+++ src/app/Campaigns/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1 style={{ marginLeft: '40px', marginRight: '40px', marginTop: '40px', fontWeight: 'bold', fontSize: '40px' }}>
+  Compaigns
+</h1>
+  )
+}
+
+export default page
---
+++ src/app/User/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1 style={{ marginLeft: '40px', marginRight: '40px', marginTop: '40px', fontWeight: 'bold', fontSize: '40px' }}>
+  User
+</h1>
+  )
+}
+
+export default page
