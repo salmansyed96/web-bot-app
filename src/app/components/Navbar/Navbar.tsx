1e84df21508cf073f792606a4de88d64d4ceff5e
Syed Mohammad Salman <124539970+salmansyed96@users.noreply.github.com>
2024-05-14 18:04:27 +0530

    add logo sections and profile sextions

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='bg-green-700 py-4 text-center'>
-     Nav Bar
-    </div>
+    <div className='bg-green-700 py-4 text-left'>
+  <img src='logo.png' alt='Logo' className='h-8 w-auto inline-block' />
+  <span className='text-white ml-2 text-xl font-bold'>WhatsBot</span>
+  <img src='profile_icon.png' alt='Profile' className='h-8 w-auto inline-block float-right mr-4' />
+</div>
+
   )
 }
 
